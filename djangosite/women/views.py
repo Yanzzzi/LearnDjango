ea078fcd53ca40469ef6f5ea01eb4e82cd80ec2b
Yanzzzi <nekit.d-k@yandex.ru>
2022-09-04 17:00:32 +0400

    динамические ссылки на url'ы

--- djangosite/women/views.py
+++ djangosite/women/views.py
@@ -2,22 +2,36 @@ from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect
 from .models import *
 
-menu = ['О сайте', 'Добавить статью', 'Обратная свзяь', 'Войти']
+menu = [
+    {'title': 'О сайте', 'url_name': 'about'},
+    {'title': 'Добавить статью', 'url_name': 'add_page'},
+    {'title': 'Обратная связь', 'url_name': 'contact'},
+    {'title': 'Войти', 'url_name': 'login'}
+]
+
 def index(request):
     posts = Women.objects.all()
-    return render(request, 'women/index.html', {'title': 'Главная страница', 'menu': menu, 'posts': posts})
+    context = {
+        'posts': posts,
+        'menu': menu,
+        'title': 'Главная страница'
+    }
+    return render(request, 'women/index.html', context=context)
 
 def about(request):
     return render(request, 'women/about.html', {'title': 'О сайте', 'menu': menu})
 
-def categories(request, catid):
-    print(request.GET)
-    return HttpResponse(f'<h1>Статьи по категориям</h1><p>{catid}</p>')
+def addpage(request):
+    return HttpResponse('Добавление статьи')
 
-def archive(request, year):
-    if int(year) > 2022:
-        return redirect('home', permanent=True)
-    return HttpResponse(f'<h2> Entered year is {year}</h2>')
+def contact(request):
+    return HttpResponse('Обратная связь')
+
+def login(request):
+    return HttpResponse('Авторизация')
+
+def show_post(request, post_id):
+    return HttpResponse(f'Статья с id {post_id}')
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1> pishov v sraku </h1>')
