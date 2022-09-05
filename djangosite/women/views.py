a463473eb04114b9313b5288c7be7eefeaf663b1
Yanzzzi <nekit.d-k@yandex.ru>
2022-09-05 16:56:29 +0400

    Вторичные ключи

--- djangosite/women/views.py
+++ djangosite/women/views.py
@@ -11,10 +11,13 @@ menu = [
 
 def index(request):
     posts = Women.objects.all()
+    cats = Category.objects.all()
     context = {
         'posts': posts,
+        'cats': cats,
         'menu': menu,
-        'title': 'Главная страница'
+        'title': 'Главная страница',
+        'cat_selected': 0
     }
     return render(request, 'women/index.html', context=context)
 
@@ -32,6 +35,20 @@ def login(request):
 
 def show_post(request, post_id):
     return HttpResponse(f'Статья с id {post_id}')
+
+def show_category(request, cat_id):
+    posts = Women.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+    if len(posts) == 0:
+        raise Http404()
+    context = {
+        'posts': posts,
+        'cats': cats,
+        'menu': menu,
+        'title': 'отображение по рубрикам',
+        'cat_selected': cat_id
+    }
+    return render(request, 'women/index.html', context=context)
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1> pishov v sraku </h1>')
