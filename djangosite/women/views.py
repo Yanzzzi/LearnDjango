b5b467647320d7baa4b2e8422ab3b67e49f25f22
Yanzzzi <nekit.d-k@yandex.ru>
2022-09-08 15:05:43 +0400

    Классы представлений: ListView, DetailView, CreateView

--- djangosite/women/views.py
+++ djangosite/women/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
 
 from .forms import *
 from .models import *
+
+from django.views.generic import ListView, DeleteView, CreateView
 
 menu = [
     {'title': 'О сайте', 'url_name': 'about'},
@@ -11,38 +14,50 @@ menu = [
     {'title': 'Войти', 'url_name': 'login'}
 ]
 
-def index(request):
-    # posts = Women.objects.all()
-    context = {
-        # 'posts': posts,
-        'title': 'Главная страница',
-        'cat_selected': 0
-    }
-    return render(request, 'women/index.html', context=context)
+class WomenHome(ListView):
+    model = Women
+    template_name = 'women/index.html'
+    context_object_name = 'posts'
 
-def show_category(request, cat_slug):
-    # posts = Women.objects.filter(cat_id=cat_id)
-    # if len(posts) == 0:
-    #     raise Http404()
-    context = {
-        # 'posts': posts,
-        'title': 'отображение по рубрикам',
-        'cat_selected': cat_slug
-    }
-    return render(request, 'women/index.html', context=context)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['title'] = 'Главная2'
+        context['cat_selected'] = 0
+        return context
+
+    def get_queryset(self):
+        return Women.objects.filter(is_published=True)
+
+class WomenCategory(ListView):
+    model = Women
+    template_name = 'women/index.html'
+    context_object_name = 'posts'
+    allow_empty = False
+
+    def get_queryset(self):
+        return Women.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['title'] = 'Категория - ' + str(context['posts'][0].cat)
+        context['cat_selected'] = context['posts'][0].cat_id
+        return context
 
 def about(request):
     return render(request, 'women/about.html', {'title': 'О сайте', 'menu': menu})
 
-def addpage(request):
-    if request.method == 'POST':
-        form = AddPostForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    else:
-        form = AddPostForm()
-    return render(request, 'women/addpage.html', {'form': form, 'menu': menu, 'title': 'Добавление статьи'})
+class AddPage(CreateView):
+    form_class = AddPostForm
+    template_name = 'women/addpage.html'
+    success_url = reverse_lazy('home') #если хочу редиректить на определенную страницу после сохдания записи
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # context['menu'] = menu
+        context['title'] = 'Добавление записи'
+        return context
 
 def contact(request):
     return HttpResponse('Обратная связь')
@@ -50,17 +65,18 @@ def contact(request):
 def login(request):
     return HttpResponse('Авторизация')
 
-def show_post(request, post_slug):
-    post = get_object_or_404(Women, slug=post_slug)
+class ShowPost(DeleteView):
+    model = Women
+    template_name = 'women/post.html'
+    slug_url_kwarg = 'post_slug'
+    context_object_name = 'post'
 
-    context = {
-        'post': post,
-        'menu': menu,
-        'title': post.title,
-        'cat_selected': post.cat_id
-    }
-
-    return render(request, 'women/post.html', context=context)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # context['menu'] = menu
+        context['title'] = context['post']
+        # context['cat_selected'] = context['posts'][0].cat_id
+        return context
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1> pishov v sraku </h1>')
