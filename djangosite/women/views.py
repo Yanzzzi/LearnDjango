72c9970aed09b057b83595439033bb806dd82daa
Yanzzzi <nekit.d-k@yandex.ru>
2022-09-12 18:09:12 +0400

    Добавил регистрацию новых пользователей

--- djangosite/women/views.py
+++ djangosite/women/views.py
@@ -70,3 +70,13 @@ class ShowPost(DataMixin, DeleteView):
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1> pishov v sraku </h1>')
+
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name = 'women/register.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Регистрация')
+        return dict(list(context.items()) + list(c_def.items()))
