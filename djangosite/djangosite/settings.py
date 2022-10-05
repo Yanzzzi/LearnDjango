c898e6b66af4c4b02b3ffc63bffd8eea4597e880
Nikita Dmitrienko <112479647+Yanzzzi@users.noreply.github.com>
2022-10-05 13:04:58 +0400

    Update settings.py

--- djangosite/djangosite/settings.py
+++ djangosite/djangosite/settings.py
@@ -23,7 +23,7 @@ mimetypes.add_type("application/javascript", ".js", True)
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f++9u&bsbr7&_$yftb*^xzfm$#l*9oj29^7zs17llz=t7fq$@8'
+SECRET_KEY = 'secretKey'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
